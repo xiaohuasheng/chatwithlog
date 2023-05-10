7cacfd295002f69763047146fdb92bcd801535bd
huangfangming <huangfangming@ones.ai>
2023-05-10 10:49:37 +0800

    feature: add chart and element plus and axios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Echart/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Echart from './components/echart.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Echart
   }
 }
 </script>
